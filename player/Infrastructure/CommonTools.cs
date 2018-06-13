178b56c8a79508a9d336e57e5b25cbde3ca022ab
eyalmiz <eyalmiz@T-2-PRJ-11-WW>
2018-06-13 12:41:29 +0300

    add common tools

---
+++ player/Infrastructure/CommonTools.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RoboCup.Infrastructure
+{
+    public static class CommonTools
+    {
+
+        //get relative angle to target x,y
+        public static double GetRelativeAngle(float? bodyAngle, PointF? pos, float trg__pos_x, float trg__pos_y)
+        {
+
+            double direction = Math.Atan2(trg__pos_y - pos.Value.Y, trg__pos_x - pos.Value.X) / Math.PI * 360;
+            double angleToTurn = direction - bodyAngle.Value;
+
+            if (angleToTurn > 180)
+            {
+                return angleToTurn - 360;
+            }
+            else if (angleToTurn < -180)
+            {
+                return angleToTurn + 360;
+
+            }
+            return angleToTurn;
+        }
+    }
+}

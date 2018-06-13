5b6be6de43a5cc5f4f7724ff4ce9647da0adda35
sagia <sagia@T-2-PRJ-15-WW>
2018-06-13 12:04:04 +0300

    adding consts

--- player/Entities/Consts.cs
+++ player/Entities/Consts.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 
@@ -9,5 +10,13 @@ namespace RoboCup.Entities
     {
         public const float KICKABLE_AREA = 0.8f + 0.085f + 1f;
         public const int WAIT_FOR_MSG_TIME = 10;
+
+        public readonly PointF plt = new PointF(-36, 20);
+        public readonly PointF plb = new PointF(-36, -20);
+        public readonly PointF prt = new PointF(36, 20);
+        public readonly PointF prb = new PointF(36, -20);
+
+        public readonly PointF center = new PointF(0, 0);
+
     }
 }
